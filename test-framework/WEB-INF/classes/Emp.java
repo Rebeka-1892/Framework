9d6eabd439aecf34e37719d523f2896cd2880782
Rebeka-1892 <etu1892@gmail.com>
2023-04-25 13:05:00 +0300

    Sprint7

--- test-framework/WEB-INF/classes/Emp.java
+++ test-framework/WEB-INF/classes/Emp.java
@@ -4,24 +4,48 @@ import annotation.Urls;
 import utilitaire.ModelView;
 
 public class Emp {
-    int id;
+    int age;
     String nom;
 
-    public Emp(int id, String nom) {
-        this.id = id;
+    public Emp(int age, String nom) {
+        this.age = age;
         this.nom = nom;
     }
     
     public Emp() {
     }
 
-    public String getNom(){
+    public String getnom(){
         return this.nom;
+    }
+
+    public int getage(){
+        return this.age;
+    }
+
+    public void setnom(String nom){
+        this.nom = nom;
+    }
+
+    public void setage(int age){
+        this.age = age;
     }
     
     @Urls(url="emp_all")
     public ModelView findAll(){
         Emp employe = new Emp(7, "Olona Miasa");
+
+        ModelView mod = new ModelView();
+        mod.AddItem("list_emp", employe);
+
+        ModelView modele = new ModelView("emp_all.jsp", mod.getData());
+        return modele;
+    }
+
+    @Urls(url="emp_save")
+    public ModelView Save(){
+        System.out.println("Age: " + getage());
+        Emp employe = new Emp(getage(), getnom());
 
         ModelView mod = new ModelView();
         mod.AddItem("list_emp", employe);
